06f2019f30d5057da38372334454966b7298da37
Ron Hsiao <blurryeyes14@gmail.com>
2017-04-09 11:52:52 +0800

    ptt test

--- src/crawler/example/BasicExample.java
+++ src/crawler/example/BasicExample.java
@@ -2,6 +2,9 @@ package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
 import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.select.Elements;
+
+import static sun.plugin.javascript.navig.JSType.Element;
 
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
@@ -20,23 +23,30 @@ public class BasicExample {
 		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
 
 		// 遠端資料路徑
-		String uri = "http://.../";
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1491542146.A.CCE.html";
 
-		System.out.println(
-				CrawlerPack.start()
-				
+
+		Elements test1;
+		//System.out.println(
+			  test1 = CrawlerPack.start()
+
 				// 參數設定
-			    //.addCookie("key","value")	// 設定cookie
+			    .addCookie("over18","1")	// 設定cookie
 				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
 				
 				// 選擇資料格式 (三選一)
-				.getFromJson(uri)
-			    //.getFromHtml(uri)
+				//.getFromJson(uri)
+			    .getFromHtml(uri)
 			    //.getFromXml(uri)
 			    
 			    // 這兒開始是 Jsoup Document 物件操作
-			    .select(".css .selector ")
+			    .select("#main-content.bbs-screen.bbs-content");
+
+				//.select("body>div>div.bbs-screen.bbs-content span:containsOwn(噓)")
+				//所有噓文
 			    
-		);
+		//);
+		test1.select("div").remove();
+		System.out.println(test1);
 	}
 }
